6a8663d8935872aeaf3ccca2063d9f60542370c1
anhle1512001@gmail.com <anhle1512001@gmail.com>
2022-06-21 13:07:12 +0700

    admin books store

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Category, Book, Publisher, Author, BookCategory
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'parent']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Publisher, PublisherAdmin)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'create_at', 'update_at']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity',
+                    'total_review', 'rating', 'state',
+                    'author', 'author', 'publisher',
+                    'sold_quantity', 'public_year',
+                    'language', 'weight', 'discount']
+
+    list_filter = ['state']
+    list_editable = ['price', 'quantity', 'state']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Book, BookAdmin)
